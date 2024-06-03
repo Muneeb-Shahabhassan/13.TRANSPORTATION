f22dc3ce9e2f7c606bef96252c9807b403273be9
Muneeb-Shahabhassan <muneebsahabhassan098890@gmail.com>
2024-06-03 12:12:36 -0700

    13.TRANSPORTATION

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let transportation = [`Mercedes`, `Audi`, `BMW`, `Ferrari`, `Rolls Royce`];
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`I would like to own a ` + transportation[i]);
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let transportation: string[] =[`Mercedes`,`Audi`,`BMW`,`Ferrari`,`Rolls Royce`]
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`I would like to own a ` + transportation[i]);
+}
